ed7b6e4fb920c6b4082b28363132c5a146cc82a3
Rando Aljaste <31949050+randoaljaste@users.noreply.github.com>
2018-03-13 09:32:13 +0200

    uus

---
+++ src/main/java/kabe/RandoKabeLaud.java
@@ -0,0 +1,6 @@
+package kabe;
+public class RandoKabeLaud implements KabeLaud{
+   public int veergKoodiks(char veerg){
+     return -1;
+   }
+}
--- src/test/java/kabe/Testid1.java
+++ src/test/java/kabe/Testid1.java
@@ -2,9 +2,13 @@ package kabe;
 import org.junit.*;
 import static org.junit.Assert.*;
 public class Testid1{
+   KabeLaud laud=new RandoKabeLaud();
    @Test
    public void test1(){
       assertEquals(3, 1+2);
    }
+   @Test
+   public void test2(){
+	   
+   }
 }
-
